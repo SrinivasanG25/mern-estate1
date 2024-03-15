09e23574697e9b9ef8bd44d62b59125db26991ff
Srinivasan G <ganesansrinivasan558@gmail.com>
2024-03-15 09:09:48 +0530

    Add create listing api route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,21 +2,21 @@ import {FaSearch} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 export default function Header() {
-  const {currentUser} = useSelector(state=>state.user)
+  const {currentUser} = useSelector((state)=>state.user)
   return (
-    <header className='bg-red-100 shadow-md'>
+    <header className='bg-yellow-100 shadow-md'>
         <div className='flex justify-between item-center max-w-6xl mx-auto p-3'>
           <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-red-400'>Srini</span>
+            <span className='text-red-400'>Real</span>
             <span className='text-red-800'>Estate</span>
         </h1>
           </Link>
         
         
-        <form className='bg-red-50 p-3 rounded-lg flex items-center' >
+        <form className='bg-yellow-50 p-3 rounded-lg flex items-center' >
             <input type="text" placeholder='Search.....' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-            <FaSearch className='text-red-600'></FaSearch>
+            <FaSearch className='text-yellow-600'></FaSearch>
         </form>
         <ul className='flex gap-4'>
         <Link to ='/'>  <li className='hidden sm:inline text-red-700 hover:underline'>Home</li></Link>
