5a31ad1871d6c4504cc5522213098aa510e5f3aa
Srinivasan G <ganesansrinivasan558@gmail.com>
2024-03-21 22:25:20 +0530

    complete about page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,38 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Contact({listing}) {
-    const [landlore,setlandlore] = useState(null);
-    const [message,setMessage] = useState('');
-    const onChange = (e) => {
-        setMessage(e.target.value)
-    }
+export default function Contact({ listing }) {
+  const [landlord, setLandlord] = useState('null');
+  const [message, setMessage] = useState('');
+  const onChange = (e) => {
+    setMessage(e.target.value);
+  };
 
-
-    useEffect(()=>{
-        const fetchLandlore = async () => {
-            try {
-               const res = await fetch(`/api/user/${listing.userRef}`) ;
-               const data = await res.json();
-               setlandlore(data);
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        fetchLandlore()
-
-    },[listing.userRef])
+  useEffect(() => {
+    const fetchLandlord = async () => {
+      try {
+        const res = await fetch(`/api/user/${listing.userRef}`);
+        const data = await res.json();
+        setLandlord(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchLandlord();
+  }, [listing.userRef]);
   return (
     <>
-      {landlore && (
+      {landlord && (
         <div className='flex flex-col gap-2'>
-            <p>Contact <span className='font-semibold'>{landlore.username}</span> for <span className='font-semibold'>{listing.name.toLowerCase()} </span></p>
-            <textarea name='message' id='message' rows='2' value={message} onChange={onChange} placeholder='Enter your message here...' className='w-full border p-3 rounded-lg'></textarea>
-            <Link to={`mailto:${landlore.email}?subject=Regarding ${listing.name}&body=${message}`}  className='bg-red-700 text-white w-full p-3 text-center rounded-lg uppercase hover:opacity-70'>
-                Send message
-            </Link>
+          <p>
+            Contact <span className='font-semibold'>{landlord.username}</span>{' '}
+            for{' '}
+            <span className='font-semibold'>{listing.name.toLowerCase()}</span>
+          </p>
+          <textarea
+            name='message'
+            id='message'
+            rows='2'
+            value={message}
+            onChange={onChange}
+            placeholder='Enter your message here...'
+            className='w-full border p-3 rounded-lg'
+          ></textarea>
+
+          <Link
+          to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+          className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
+          >
+            Send Message          
+          </Link>
         </div>
       )}
     </>
-  )
+  );
 }
